11764cc09aa8421dbe9c63633048a2eece806186
proyogram <proyogram@gmail.com>
2024-05-19 22:13:39 +0900

    コメントを追加

--- lib/cw_alarm/main.ts
+++ lib/cw_alarm/main.ts
@@ -8,6 +8,7 @@ export class CwAlarmStack extends cdk.Stack {
     super(scope, id, props);
 
     const sns = new SnsConstruct(this, 'SnsConstruct', snsProps);
+    // A SNS topic is needed at the forth argument.
     const cw = new CwConstruct(this, 'CwConstruct', cwProps, sns.alarmTopic);
   }
 }
